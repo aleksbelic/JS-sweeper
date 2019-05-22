e0521f45d39b212a03ffb7dd9bad4438f12e71f3
Aleksandar Belic <aleksandar.belic@netcare.de>
2019-05-22 11:01:32 +0200

    Adding method to generate custom-size board.

--- js-sweeper.js
+++ js-sweeper.js
@@ -5,4 +5,18 @@ var board = {
 	}
 };
 
-console.log("loaded");
+function generateBoard(width = board.size.width, height = board.size.height) {
+	let boardContainer = document.getElementById("js-sweeper");
+	for (i = 0; i < height; i++) {
+		let boardRow = document.createElement('div');
+		boardRow.className = "row";
+		boardContainer.appendChild(boardRow);
+		for (j = 0; j < width; j++) {
+			let cell = document.createElement('div');
+			cell.className = "cell";
+			let cellText = document.createTextNode("X");
+			cell.appendChild(cellText);
+			boardRow.appendChild(cell);
+		}
+	}
+}
